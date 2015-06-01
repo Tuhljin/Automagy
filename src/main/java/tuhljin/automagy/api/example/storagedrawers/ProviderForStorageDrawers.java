f0caf5b95d37fb781215eb744d8ad77bfb42756e
Tuhljin <Tuhljin@users.noreply.github.com>
2015-06-01 14:21:40 -0600

    Update for v0.24.1
    Fix Inventarium ignoring compacting drawers.

--- src/main/java/tuhljin/automagy/api/example/storagedrawers/ProviderForStorageDrawers.java
+++ src/main/java/tuhljin/automagy/api/example/storagedrawers/ProviderForStorageDrawers.java
@@ -10,12 +10,13 @@ import tuhljin.automagy.api.inventarium.IInventariumContentsProvider;
 
 import com.jaquadro.minecraft.storagedrawers.api.storage.IDrawer;
 import com.jaquadro.minecraft.storagedrawers.api.storage.IDrawerGroup;
+import com.jaquadro.minecraft.storagedrawers.api.storage.IFractionalDrawer;
 
 public class ProviderForStorageDrawers implements IInventariumContentsProvider {
 
 	public static final int PRIORITY = 120;
 
-	ItemStack dummyItemOnePerStack = new ItemStack(Items.wooden_sword);
+	private static ItemStack dummyItemOnePerStack = new ItemStack(Items.wooden_sword);
 
 	@Override
 	public boolean canHandleTile(TileEntity te) {
@@ -27,9 +28,20 @@ public class ProviderForStorageDrawers implements IInventariumContentsProvider {
 		int slots = 0;
 		IDrawerGroup group = (IDrawerGroup) te;
 		for (int i = 0; i < group.getDrawerCount(); i++) {
-			IDrawer drawer = group.getDrawer(i);
-			if (drawer != null)  slots += drawer.getMaxCapacity(dummyItemOnePerStack);
-			//ItemStack stack = drawer.getStoredItemPrototype();
+			if (group.isDrawerEnabled(i)) {
+				IDrawer drawer = group.getDrawer(i);
+				if (drawer != null) {
+					if (drawer.isEmpty()) {
+						slots += drawer.getMaxCapacity(dummyItemOnePerStack);
+					} else {
+						if (drawer instanceof IFractionalDrawer && ((IFractionalDrawer)drawer).getConversionRate() != 1) {
+							// If this is a compacting drawer, then only include slots for the most compact item.
+							continue;
+						}
+						slots += (drawer.getMaxCapacity() / drawer.getStoredItemStackSize());
+					}
+				}
+			}
 		}
 		return slots;
 	}
@@ -39,10 +51,12 @@ public class ProviderForStorageDrawers implements IInventariumContentsProvider {
 		List<ItemStack> list = new ArrayList<ItemStack>();
 		IDrawerGroup group = (IDrawerGroup) te;
 		for (int i = 0; i < group.getDrawerCount(); i++) {
-			IDrawer drawer = group.getDrawer(i);
-			if (drawer != null) {
-				ItemStack stack = drawer.getStoredItemCopy();
-				if (stack != null)  list.add(stack);
+			if (group.isDrawerEnabled(i)) {
+				IDrawer drawer = group.getDrawer(i);
+				if (drawer != null) {
+					ItemStack stack = drawer.getStoredItemCopy();
+					if (stack != null)  list.add(stack);
+				}
 			}
 		}
 		return list.toArray(new ItemStack[list.size()]);
